3065cb3b57881b94dfe21cb47dd49021654c71e1
jida <jakazzy@gmail.com>
2019-01-04 17:28:02 +0000

    add create question, show a question and show all questions

---
+++ test/questionsControllerTest.js
@@ -0,0 +1,36 @@
+let chai = require("chai");
+let expect = chai.expect;
+let should = chai.should();
+let chaiHTTP = require("chai-http");
+let server = require("../index");
+let faker = require("faker");
+let questions = require("./../app/dataStore/questionsTable");
+
+chai.use(chaiHTTP);
+
+describe("QuestionsController", function() {
+
+    describe("/api/v1/questions", function() {
+        it("should create a question", function(done) {
+            const validQuestion = {
+                createdOn: faker.date.recent(),
+                userID: 1,
+                meetupID: 1,
+                title: faker.lorem.sentence(),
+                body: faker.lorem.words(),
+                upvotes: faker.random.number(),
+                downvotes: faker.random.number()
+            };
+
+            chai.request(server)
+                .post("/api/v1/questions")
+                .send(validQuestion)
+                .end(function(err, res) {
+                    res.status.should.be.eql(201);
+                })
+            done();
+        });
+
+
+    });
+})
